e6d4ba17a649f23795eb37a1c4df283c2070cba4
Shane Parker <shane.tim.parker@ea.com>
2022-01-15 03:43:52 -0500

    Made (S)GRANTS self-reliant.

--- scripts/manager_effect_cg.lua
+++ scripts/manager_effect_cg.lua
@@ -20,11 +20,7 @@ function onInit()
 	getEffectsByTypeOriginal = EffectManager5E.getEffectsByType;
 	EffectManager5E.getEffectsByType = getEffectsByType;
 
-	if EffectsManagerBCE then
-		EffectsManagerBCE.registerBCETag("GRANTS", EffectsManagerBCE.aBCESourceMattersOptions);
-		EffectsManagerBCE.registerBCETag("SGRANTS", EffectsManagerBCE.aBCESourceMattersOptions);
-		EffectsManagerBCE.setCustomProcessTurnStart(processEffectTurnStart);
-	end
+	CombatManager.setCustomInitChange(handleInitChange);
 end
 
 function setActiveActor(rActor)
@@ -84,50 +80,65 @@ function replaceResourceValue(s, sValue, bStatic, fGetValue)
 	return s;
 end
 
-function processEffectTurnStart(rSource)
-	local aTags = {"GRANTS"};
-	local tMatch = EffectsManagerBCE.getEffects(rSource, aTags, rSource);
-	for _,rEffect in pairs(tMatch) do
-		if (rEffect.sTag == "GRANTS") and (rEffect.sSource ~= "") then
-			local rSourceEffect = ActorManager.resolveActor(rEffect.sSource);
-			processGrant(rEffect.sLabel, rSourceEffect, "GRANTS");
-		end
-	end
-	
-	aTags = {"SGRANTS"};
-	for _, nodeCT in pairs(CombatManager.getCombatantNodes()) do
-		local rActor = ActorManager.resolveActor(nodeCT);
-		if rActor.sCTNode ~= rSource.sCTNode then
-			tMatch = EffectsManagerBCE.getEffects(rActor, aTags, rSource);
-			for _,rEffect in pairs(tMatch) do
-				if rEffect.sTag == "SGRANTS" then
-					processGrant(rEffect.sLabel, rSource, "SGRANTS");
+function handleInitChange(nodeOldCT, nodeNewCT)
+	local aEntries = CombatManager.getSortedCombatantList();
+	for i = 1,#aEntries do
+		local nodeActor = aEntries[i];
+		for _,nodeEffect in pairs(DB.getChildren(nodeActor, "effects")) do
+			local nActive = DB.getValue(nodeEffect, "isactive", 0);
+			if nActive ~= 0 then
+				local rSource = ActorManager.resolveActor(DB.getValue(nodeEffect, "source_name"));
+				local nodeSource = ActorManager.getCTNode(rSource);
+				if not nodeSource then
+					break;
+				end
+
+				local sEffect;
+				if nodeActor == nodeNewCT then
+					sEffect = "GRANTS";
+				elseif nodeSource == nodeNewCT then
+					sEffect = "SGRANTS";
+				else
+					break;
+				end
+
+				local sEffName = DB.getValue(nodeEffect, "label", "");
+				local aEffectComps = EffectManager.parseEffect(sEffName);
+				for _,sEffectComp in ipairs(aEffectComps) do
+					local rEffectComp = EffectManager5E.parseEffectComp(sEffectComp);
+					if rEffectComp.type == "IFT" then
+						break;
+					elseif rEffectComp.type == "IF" then
+						local rActor = ActorManager.resolveActor(nodeActor);
+						if not EffectManager5E.checkConditional(rActor, nodeEffect, rEffectComp.remainder) then
+							break;
+						end
+					elseif rEffectComp.type == sEffect then
+						if nActive == 2 then
+							DB.setValue(nodeEffect, "isactive", "number", 1);
+						else
+							processGrant(rSource, rEffectComp)
+						end
+					end
 				end
 			end
 		end
 	end
-	return true;
 end
 
-function processGrant(sEffect, rSourceEffect, sEffectType)
-	local aEffectComps = EffectManager.parseEffect(sEffect);
-	for _,sEffectComp in ipairs(aEffectComps) do
-		local rEffectComp = EffectManager5E.parseEffectComp(sEffectComp);
-		if rEffectComp.type == sEffectType then
-			for _,sResource in ipairs(rEffectComp.remainder) do
-				local rAction = {};
-				rAction.type = "resource";
-				rAction.operation = "gain";
-				rAction.label = "Ongoing Resource Grant";
-				rAction.resource = sResource;
-				rAction.modifier = rEffectComp.mod;
-				rAction.dice = rEffectComp.dice;
+function processGrant(rSource, rEffectComp)
+	for _,sResource in ipairs(rEffectComp.remainder) do
+		local rAction = {};
+		rAction.type = "resource";
+		rAction.operation = "gain";
+		rAction.label = "Ongoing Resource Grant";
+		rAction.resource = sResource;
+		rAction.modifier = rEffectComp.mod;
+		rAction.dice = rEffectComp.dice;
 
-				rRoll = ActionResource.getRoll(rSourceEffect, rAction);
-				if rRoll then
-					ActionsManager.performMultiAction(nil, rSourceEffect, rRoll.sType, {rRoll});
-				end
-			end
+		rRoll = ActionResource.getRoll(rSource, rAction);
+		if rRoll then
+			ActionsManager.performMultiAction(nil, rSource, rRoll.sType, {rRoll});
 		end
 	end
 end
